c9ad5c6014c284d9fba1de84c15389d376416cfd
vishnu291093 <vishnu_291093@hotmail.com>
2016-06-15 12:36:28 -0400

    Programmed till make plan

--- src/A_star_ros.cpp
+++ src/A_star_ros.cpp
@@ -15,39 +15,80 @@
 
 #include <pluginlib/class_list_macros.h>
 //register this planner as a BaseGlobalPlanner plugin
-PLUGINLIB_EXPORT_CLASS(A_star_ros::AstarROS, nav_core::BaseGlobalPlanner)
+PLUGINLIB_EXPORT_CLASS(Astar_planner::AstarPlannerROS, nav_core::BaseGlobalPlanner)
 
-namespace A_star_ros
+int value;
+int mapSize;
+bool* OGM;
+
+namespace Astar_planner
 {
 
 //Default Constructor
-AstarROS::AstarROS()
+AstarPlannerROS::AstarPlannerROS()
 {
 
 }
-AstarROS::AstarROS(ros::NodeHandle &nh)
+AstarPlannerROS::AstarPlannerROS(ros::NodeHandle &nh)
 {
   ROSNodeHandle = nh;
 
 }
 
-AstarROS::AstarROS(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
+AstarPlannerROS::AstarPlannerROS(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
 {
   initialize(name, costmap_ros);
 }
-void AstarROS::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
+
+void RAstarPlannerROS::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
 {
 
   if (!initialized_)
   {
+    costmap_ros_ = costmap_ros;
+    costmap_ = costmap_ros_->getCostmap();
+
+    ros::NodeHandle private_nh("~/" + name); //not sure where this is being used
+
+    originX = costmap_->getOriginX();
+    originY = costmap_->getOriginY();
+
+
+
+	width = costmap_->getSizeInCellsX();
+	height = costmap_->getSizeInCellsY();
+	resolution = costmap_->getResolution();
+	mapSize = width*height;
+    //tBreak = 1+1/(mapSize);
+	value =0;
+
+
+	OGM = new bool [mapSize];
+    for (unsigned int iy = 0; iy < costmap_->getSizeInCellsY(); iy++)
+    {
+      for (unsigned int ix = 0; ix < costmap_->getSizeInCellsX(); ix++)
+      {
+        unsigned int cost = static_cast<int>(costmap_->getCost(ix, iy));
+        //cout<<cost;
+        if (cost == 0)
+          OGM[iy*width+ix]=true;
+        else
+          OGM[iy*width+ix]=false;
+      }
+    }
+
+    ROS_INFO("RAstar planner initialized successfully");
+    initialized_ = true;
   }
-      ROS_WARN("This planner has already been initialized... doing nothing");
+  else
+    ROS_WARN("This planner has already been initialized... doing nothing");
 }
 
-bool AstarROS::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal,
+bool RAstarPlannerROS::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal,
                              std::vector<geometry_msgs::PoseStamped>& plan)
 {
-if (!initialized_)
+
+  if (!initialized_)
   {
     ROS_ERROR("The planner has not been initialized, please call initialize() to use the planner");
     return false;
@@ -64,3 +105,117 @@ if (!initialized_)
               costmap_ros_->getGlobalFrameID().c_str(), goal.header.frame_id.c_str());
     return false;
   }
+
+  tf::Stamped < tf::Pose > goal_tf;
+  tf::Stamped < tf::Pose > start_tf;
+
+  poseStampedMsgToTF(goal, goal_tf);
+  poseStampedMsgToTF(start, start_tf);
+
+  // convert the start and goal positions
+
+  float startX = start.pose.position.x;
+  float startY = start.pose.position.y;
+
+  float goalX = goal.pose.position.x;
+  float goalY = goal.pose.position.y;
+
+  //getCorrdinate(startX, startY);
+  //getCorrdinate(goalX, goalY);
+/*
+  int startCell;
+  int goalCell;
+
+  if (isCellInsideMap(startX, startY) && isCellInsideMap(goalX, goalY))
+  {
+    startCell = convertToCellIndex(startX, startY);
+
+    goalCell = convertToCellIndex(goalX, goalY);
+
+MyExcelFile << startCell <<"\t"<< start.pose.position.x <<"\t"<< start.pose.position.y <<"\t"<< goalCell <<"\t"<< goal.pose.position.x <<"\t"<< goal.pose.position.y;
+
+  }
+  else
+  {
+    ROS_WARN("the start or goal is out of the map");
+    return false;
+  }
+
+  /////////////////////////////////////////////////////////
+
+  // call global planner
+
+  if (isStartAndGoalCellsValid(startCell, goalCell)){
+
+        vector<int> bestPath;
+	bestPath.clear();
+
+    bestPath = RAstarPlanner(startCell, goalCell);
+
+//if the global planner find a path
+    if ( bestPath.size()>0)
+    {
+
+// convert the path
+
+      for (int i = 0; i < bestPath.size(); i++)
+      {
+
+        float x = 0.0;
+        float y = 0.0;
+
+        int index = bestPath[i];
+
+        convertToCoordinate(index, x, y);
+
+        geometry_msgs::PoseStamped pose = goal;
+
+        pose.pose.position.x = x;
+        pose.pose.position.y = y;
+        pose.pose.position.z = 0.0;
+
+        pose.pose.orientation.x = 0.0;
+        pose.pose.orientation.y = 0.0;
+        pose.pose.orientation.z = 0.0;
+        pose.pose.orientation.w = 1.0;
+
+        plan.push_back(pose);
+      }
+
+
+	float path_length = 0.0;
+
+	std::vector<geometry_msgs::PoseStamped>::iterator it = plan.begin();
+
+	geometry_msgs::PoseStamped last_pose;
+	last_pose = *it;
+	it++;
+	for (; it!=plan.end(); ++it) {
+	   path_length += hypot(  (*it).pose.position.x - last_pose.pose.position.x,
+		                 (*it).pose.position.y - last_pose.pose.position.y );
+	   last_pose = *it;
+	}
+	cout <<"The global path length: "<< path_length<< " meters"<<endl;
+	MyExcelFile << "\t" <<path_length <<"\t"<< plan.size() <<endl;
+      //publish the plan
+
+      return true;
+
+    }
+
+    else
+    {
+      ROS_WARN("The planner failed to find a path, choose other goal position");
+      return false;
+    }
+
+  }
+
+  else
+  {
+    ROS_WARN("Not valid start or goal");
+    return false;
+  }
+
+}
+*/
--- src/A_star_ros.h
+++ src/A_star_ros.h
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -49,67 +50,32 @@
 #include <base_local_planner/costmap_model.h>
 
 #include <set>
-using namespace std;
-using std::string;
 
-#ifndef ASTAR_ROS_CPP
-#define ASTAR_ROS_CPP
+#ifndef ASTAR_CPP
+#define ASTAR_CPP
 
-namespace A_star_ros {
+struct cells {
+	//needs to filled out
+};
 
-class AstarROS : public nav_core::BaseGlobalPlanner {
+
+namespace Astar_planner {  //dont noe if this is needed
+
+class AstarPlannerROS : public nav_core::BaseGlobalPlanner {
 public:
 
-  AstarROS (ros::NodeHandle &); //this constructor is may be not needed
-  AstarROS ();
-  AstarROS(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+  AstarPlannerROS (ros::NodeHandle &); //this constructor is may be not needed
+  AstarPlannerROS ();
+  AstarPlannerROS(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
 
   ros::NodeHandle ROSNodeHandle;
 
-  /** overriden classes from interface nav_core::BaseGlobalPlanner **/
   void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
   bool makePlan(const geometry_msgs::PoseStamped& start,
 		const geometry_msgs::PoseStamped& goal,
 		std::vector<geometry_msgs::PoseStamped>& plan
 	       );
 
-/**
-  void getCorrdinate (float& x, float& y);
-  int convertToCellIndex (float x, float y);
-  void convertToCoordinate(int index, float& x, float& y);
-  bool isCellInsideMap(float x, float y);
-  void mapToWorld(double mx, double my, double& wx, double& wy);
-  vector<int> RAstarPlanner(int startCell, int goalCell);
-  vector<int> findPath(int startCell, int goalCell, float g_score[]);
-  vector<int> constructPath(int startCell, int goalCell, float g_score[]);
-  float calculateHCost(int cellID, int goalCell){
-  int x1=getCellRowID(goalCell);
-  int y1=getCellColID(goalCell);
-  int x2=getCellRowID(cellID);
-  int y2=getCellColID(cellID);
-    return abs(x1-x2)+abs(y1-y2);
-	//return min(abs(x1-x2),abs(y1-y2))*sqrt(2) + max(abs(x1-x2),abs(y1-y2))-min(abs(x1-x2),abs(y1-y2));
-  }
-  void addNeighborCellToOpenList(multiset<cells> & OPL, int neighborCell, int goalCell, float g_score[]);
-  vector <int> findFreeNeighborCell (int CellID);
-  bool isStartAndGoalCellsValid(int startCell,int goalCell);
-  float getMoveCost(int CellID1, int CellID2);
-  float getMoveCost(int i1, int j1, int i2, int j2);
-  bool isFree(int CellID); //returns true if the cell is Free
-  bool isFree(int i, int j);
-
-  int getCellIndex(int i,int j) //get the index of the cell to be used in Path
-  {
-   return (i*width)+j;
-  }
-  int getCellRowID(int index)//get the row ID from cell index
-  {
-     return index/width;
-  }
-  int getCellColID(int index)//get colunm ID from cell index
-  {
-    return index%width;
-  }
 
   float originX;
   float originY;
@@ -121,8 +87,7 @@ public:
   bool initialized_;
   int width;
   int height;
-**/
-};
 
-};
-#endif
+  };
+  };
+  #endif
